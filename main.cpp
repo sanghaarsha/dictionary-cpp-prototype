c18af4c3ae1c29f7f2b7002f9b9a79e44872a6f5
sanghaarsha <sanghaarsha@gmail.com>
2020-10-17 19:58:12 +0545

    favorites and history

--- main.cpp
+++ main.cpp
@@ -1,47 +1,76 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream> 
+#include <sstream>
 
 using namespace std;
 
-int main ()
+int main()
 {
   // Opening the file and declaring a string variable for lines
   ifstream file("oxford.txt");
   string line;
-  
+
   // String to display if searched word is not found
   string notFoundMsg{}, firstMsg{"Sorry, "}, lastMsg{" not found!"};
-  
+
   // Getting search keyword from the user
-  cout<<"Enter a word to search :: ";
+  cout << "Enter a word to search :: ";
   string key{};
-  getline(cin,key);
-  
+  getline(cin, key);
+
   // Converting the keyword into titlecase if it's not
-  if(key[0]>=97 && key[0]<=122) key[0] -= 32;
-  
+  if (key[0] >= 97 && key[0] <= 122)
+    key[0] -= 32;
+
   // Searching the keyword given from user in the file line-by-line
-  while (getline(file, line)) {
-  	
-  	// storing first word of line in firstWord variable
-  	stringstream ss { line };
-  	string firstWord;
-  	ss >> firstWord; 
-  
-	if(firstWord == key){
-  		cout << line << endl;
-  		notFoundMsg = "found";
-  		break;
-	}
+  while (getline(file, line))
+  {
+
+    // storing first word of line in firstWord variable
+    stringstream ss{line};
+    string firstWord;
+    ss >> firstWord;
+
+    if (firstWord == key)
+    {
+      cout << line << endl;
+
+      cout << "\n Add to favorites? : (y/n): ";
+      string addFav{};
+      cin >> addFav;
+
+      // Writing into history file
+      ofstream historyfile;
+      historyfile.open("history.txt", ios::app); //ios::app => append mode
+      historyfile << line + "\n\n";
+      historyfile.close();
+
+      // Writing into fav file
+      if (addFav == "y" || addFav == "Y")
+      {
+        ofstream favFile;
+        favFile.open("fav.txt", ios::app);
+        favFile << line + "\n\n";
+        favFile.close();
+      }
+
+      notFoundMsg = "found";
+      break;
+    }
   }
-  
+
   // if word is not found
-  if(notFoundMsg != "found"){
-  	notFoundMsg = firstMsg+"'"+key+"'"+lastMsg;
- 	cout<<notFoundMsg<<endl;
-  } 
+  if (notFoundMsg != "found")
+  {
+    notFoundMsg = firstMsg + "'" + key + "'" + lastMsg;
+    cout << notFoundMsg << endl;
+
+    ofstream historyfile;
+    historyfile.open("history.txt", ios::app);
+    historyfile << key + "\n\n";
+    historyfile.close();
+  }
 
   return 0;
 }
